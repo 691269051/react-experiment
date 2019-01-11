ed729793370ba899c40d3393c2e5e7163fa312de
liyanhui <691269051@qq.com>
2019-01-11 14:39:00 +0800

    修改webpack路径别名配置

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -2,10 +2,17 @@ const { override } = require('customize-cra')
 const addLessLoader = require('./addLess')
 const addAntd = require('./addAntd')
 const addLodash = require('./addLodash')
-const getConfig = require('./getConfig');
-const addBundleVisualizer = require('./addBundleVisualizer');
-const setHot = require('./addHot');
-// const setPathmap = require('./setPathmap');
+const getConfig = require('./getConfig')
+const addBundleVisualizer = require('./addBundleVisualizer')
+const setHot = require('./addHot')
+// const addWebpackAlias = require('./addWebpackAlias');
 // const addPostcssPlugins = require('./addPostcssPlugins');
 
-module.exports = override(addLessLoader, addAntd, addLodash, getConfig, addBundleVisualizer, setHot)
+module.exports = override(
+    addLessLoader,
+    addAntd,
+    addLodash,
+    getConfig,
+    addBundleVisualizer,
+    setHot
+)
