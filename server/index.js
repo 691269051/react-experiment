7f64e67fdb697ad4740b7e3f04ff7a161696328d
liyanhui <691269051@qq.com>
2019-02-19 15:58:25 +0800

    修改调试server.js文件的方法

--- server/index.js
+++ server/index.js
@@ -1,36 +1,41 @@
-const express = require('express'); // 项目服务端使用express框架
-const app = express();
-const path = require('path');
-const fs = require('fs');
+const express = require('express') // 项目服务端使用express框架
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const bodyParser = require('body-parser')
 
 //使用nodejs自带的http、https模块
-const http = require('http');
-const https = require('https');
+const http = require('http')
+const https = require('https')
 
 //根据项目的路径导入生成的证书文件
 const privateKey = fs.readFileSync(
     path.join(__dirname, './certificate/server.key'),
     'utf8'
-);
+)
 const certificate = fs.readFileSync(
     path.join(__dirname, './certificate/server.crt'),
     'utf8'
-);
-const credentials = { key: privateKey, cert: certificate };
+)
+const credentials = { key: privateKey, cert: certificate }
 
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+const httpServer = http.createServer(app)
+const httpsServer = https.createServer(credentials, app)
 
 //可以分别设置http、https的访问端口号
-const PORT = 8000;
-const SSLPORT = 8001;
+const PORT = 8000
+const SSLPORT = 8001
 
-app.use(express.static(path.join(process.cwd(), 'build')));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+app.use(express.static(path.join(process.cwd(), 'build')))
 
 //创建https服务器
 httpsServer.listen(SSLPORT, function() {
-    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
-});
+    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT)
+})
 
 //创建http服务器
 // httpServer.listen(PORT, function() {
@@ -41,14 +46,14 @@ httpsServer.listen(SSLPORT, function() {
 // })
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(process.cwd(), 'build', 'index.html'));
-});
+    res.sendFile(path.join(process.cwd(), 'build', 'index.html'))
+})
 
-// app.post('/gipms/mobile/person/detail', function (req, res) {
-//     console.log(req, res)
-//     res.send({ddddd:12})
-// })
-
-
-
-
+app.post('/gipms/mobile/person/detail', function(req, res) {
+    const params = req.body;
+    if(params.isTodo === 1){
+        res.send({ ddddd: 12 })
+    }else{
+        res.send({ddddd:13})
+    }
+})
--- src/shiyan.ts
+++ src/shiyan.ts
@@ -1,8 +1,12 @@
-interface Itest {
+export interface Itest {
     webName: string
     age: number
     address: string
 }
-type ant = keyof Itest
+// type ant = keyof Itest
 
-type antd = keyof Itest
+// type antd = keyof Itest
+
+export const a = {
+    b: { c: 12 },
+}
