6f9f76daf20b639ee8c00d6db6c95cfc49dc4ee6
liyanhui <691269051@qq.com>
2019-02-26 14:15:52 +0800

    修改tsconfig

--- src/pages/hookTest/index.tsx
+++ src/pages/hookTest/index.tsx
@@ -2,42 +2,63 @@
  * @Author: 李雁辉
  * @Date: 2019-02-19 16:28:59
  * @Last Modified by: 李雁辉
- * @Last Modified time: 2019-02-19 17:16:28
+ * @Last Modified time: 2019-02-26 10:53:43
  */
+import React, {
+    useDebugValue,
+    useEffect,
+    useLayoutEffect,
+    useState,
+} from 'react'
 
-import React, { useState, useDebugValue } from 'react'
+import styles from './index.module.css'
 
 interface Iprops {
     path: string
     name: string
 }
 
-const useTimeout = (friendID: string) => {
-    const [isOnline, setIsOnline] = useState<boolean>(false)
-    useDebugValue(isOnline ? 'Online' : 'Offline')
-    return isOnline ? 'Online' : 'Offline'
-}
-
 const HookTest: React.SFC<Iprops> = ({ path, name }) => {
     const [num, setNum] = useState<number>(0)
+    const [num2, setNum2] = useState<number>(0)
+    const [{ name: names, age }, setObj] = useState<{
+        name: string
+        age: number
+    }>({ name: '李雁辉', age: 18 })
 
     const setNumCb = (e: React.MouseEvent) => {
-        // const a = useTimeout('1111')
         console.log(e.target)
         setNum(num => num + 1)
     }
 
-    // 错误代码
-    if (true) {
-        const a = useTimeout('1111')
+    const setNumCb2 = (e: React.MouseEvent) => {
+        console.log(e.target)
+        setNum2(num2 => num2 + 1)
     }
 
+    if (num > 12) {
+        const [a, setA] = useState<number>(0)
+    }
+
+    useEffect(() => {
+        console.log(num, num2)
+        return () => {
+            console.log('卸载', num, num2)
+        }
+    }, [])
+
+    useLayoutEffect(() => {
+        document.title = `第 ${num} 页`
+        console.log(12)
+    }, [num])
+
     return (
-        <div
-            style={{ height: '200px', width: '200px', color: 'red' }}
-            onClick={setNumCb}
-        >
-            {num} /dd {1}
+        <div style={{ height: '200px', width: '200px', color: 'red' }}>
+            <div className={styles.title} onClick={setNumCb}>
+                {num}{' '}
+            </div>
+            <div onClick={setNumCb2}>{num2} </div>
+            {num ** num2}
         </div>
     )
 }
