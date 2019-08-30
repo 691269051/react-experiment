0c43b63cd28f3645e93304d0592e09b11679b26f
liyanhui <691269051@qq.com>
2019-08-30 17:48:45 +0800

    chore(视图): 修改test

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
  * @Author: 李雁辉
  * @Date: 2019-01-09 10:51:26
  * @Last Modified by: 李雁辉
- * @Last Modified time: 2019-02-26 14:17:21
+ * @Last Modified time: 2019-08-09 16:15:19
  */
 import './index.css'
 
@@ -14,10 +14,18 @@ import * as serviceWorker from './serviceWorker'
 import App from './routers'
 import { store } from './store'
 
+const onRenderCallback = (...arg: any[]) => {
+    console.log('arg----start----')
+    console.log(arg)
+    console.log('arg-----end---')
+}
+
 ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
-            <App />
+            <React.Profiler id="application" onRender={onRenderCallback}>
+                <App />
+            </React.Profiler>
         </React.StrictMode>
     </Provider>,
     document.getElementById('root')
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,2 @@
-// react-testing-library 将你的组件渲染给 document.body ，
-// 这将确保在每次测试后将其删除。
-import '@testing-library/react/cleanup-after-each'
 // 这会添加 jest-dom 的自定义断言
-import 'jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect'
