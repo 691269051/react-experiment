1baea3f2b0a539636d8bede90a2c1e56dcea173d
李雁辉 <691269051@qq.com>
2021-01-26 16:45:00 +0800

    chore(打包配置): 调整打包配置

--- config-overrides/getConfig.js
+++ config-overrides/getConfig.js
@@ -2,28 +2,32 @@ const fs = require('fs')
 const color = require('colors')
 const path = require('path')
 
+const isRegExp = (obj) =>
+    Object.prototype.toString.call(obj) === '[object RegExp]'
+
 function writeFile(paths, content) {
-    console.log('准备写入文件'.green)
-    fs.writeFile(path.resolve(__dirname, paths), content, function(err) {
+    console.log('准备写入log文件'.green)
+    fs.writeFile(path.resolve(__dirname, paths), content, function (err) {
         if (err) console.error(err.toString().red)
         console.log('写入成功'.blue)
     })
 }
 
-// function toJSON (key, value) {
-
-// }
-
-RegExp.prototype.toJSON = function(key) {
-    return this.toString()
-}
-
 module.exports = function getConfig(config) {
-    // 本方法打印config,env到config.overrides.json文件
+    // 本方法打印config,env到config.overrides.log文件
     writeFile(
-        `./config.overrides.${process.env.NODE_ENV}.json`,
+        `./customize-cra.${process.env.NODE_ENV}.log`,
         // fs.createReadStream()
-        JSON.stringify(config, null, 4)
+        JSON.stringify(
+            config,
+            (key, value) => {
+                if (isRegExp(value)) {
+                    return value.toString()
+                }
+                return value
+            },
+            2,
+        ),
     )
     return config
 }
