0af476ecc577dcecdecb0e9db77a972293977478
李雁辉 <691269051@qq.com>
2021-02-09 00:48:56 +0800

    feat: 添加pwa更新时提示

--- src/index.tsx
+++ src/index.tsx
@@ -55,7 +55,21 @@ ReactDOM.render(app, rootElement)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
+serviceWorkerRegistration.register({
+    onUpdate: (registration) => {
+        console.info(
+            `${registration} onUpdate registration?.waiting?.postMessage${registration?.waiting?.postMessage}`,
+        )
+        registration?.waiting?.postMessage({ type: 'SKIP_WAITING' })
+        const reload = window.confirm('网站有了新的更新,请升级!')
+        if (reload) {
+            window.location.reload()
+        }
+    },
+    onSuccess: (registration) => {
+        console.info(`${registration} onSuccess`)
+    },
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
