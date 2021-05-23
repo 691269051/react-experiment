f37057a5579caa7f2ade148f14b1941bccb8032b
李雁辉 <691269051@qq.com>
2021-05-23 16:10:21 +0800

    chore(全局): 升级依赖库

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    stories: ['../src/**/*.stories.[t|j]s[x]'],
-    addons: [
-        '@storybook/preset-create-react-app',
-        '@storybook/addon-actions',
-        '@storybook/addon-links',
-    ],
-}
